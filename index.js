b2e0b57e72b1b18a21ff325b43f988cf934734f5
shreedev44 <shreedevchambayil@gmail.com>
2025-04-17 22:51:26 +0530

    implemented endless cron job

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
-fetch("https://inker.onrender.com/health")
-	.then((res) => console.log("running successfully"))
-	.catch(() => console.log("not good"));
+setInterval(() => {
+    fetch("https://inker.onrender.com/health")
+        .then(() => console.log("running successfully"))
+        .catch(() => console.log("not good"));
+}, 600000)
